04f5a31efa2b39095944b430410ddbae6702ecb9
Francesco Levorato <git@flevour.net>
2011-12-19 21:15:07 +0100

    refactor with Expression class - base case

--- script.php
+++ script.php
@@ -1,30 +1,44 @@
 <?php
 
+class Expression
+{
+  const FORMAT_CLEAN = '%s %s %s';
+  const FORMAT_PARENTHESIS = '(%s %s %s)';
+
+  public $commands = array(
+    '+' => self::FORMAT_CLEAN,
+    '-' => self::FORMAT_CLEAN,
+    '*' => self::FORMAT_PARENTHESIS,
+    '/' => self::FORMAT_PARENTHESIS
+  );
+
+  public $left, $operand, $right;
+
+  public function __construct($pieces) {
+    list($this->left, $this->right, $this->operand) = $pieces;
+  }
+
+  public function isValid() {
+    return in_array($this->operand, array_keys($this->commands));
+  }
+
+  public function __toString() {
+    return sprintf($this->commands[$this->operand], $this->left, $this->operand, $this->right);
+  }
+}
 class Converter
 {
-
-  const FORMAT_CLEAN = '%s';
-  const FORMAT_PARENTHESIS = '(%s)';
-
   public function convert($string) {
-    $commands = array(
-      '+' => self::FORMAT_CLEAN,
-      '-' => self::FORMAT_CLEAN,
-      '*' => self::FORMAT_PARENTHESIS,
-      '/' => self::FORMAT_PARENTHESIS
-      );
-    $pieces = explode(' ', $string);
-    if (count($pieces) == 1) {
-      return $string;
-    }
-    $output = array();
-    $output[] = array_shift($pieces);
-    $output[] = $operand = array_pop($pieces);
-    $output[] = sprintf($commands[$operand], $this->convert($this->render($pieces)));
-    return $this->render($output);
+    $pieces = $this->split($string);
+    $expression = new Expression($pieces);
+    return $expression;
   }
 
   public function render(array $output) {
     return implode(' ', $output);
+  }
+
+  public function split($string) {
+    return explode(' ', $string);
   }
 }
--- test.php
+++ test.php
@@ -11,16 +11,30 @@ class ScriptTest extends PHPUnit_Framework_TestCase
   public function testConversion($infix, $expectation) {
     $converter = new Converter();
 
-    $this->assertEquals($expectation, $converter->convert($infix));
+    $this->assertEquals($expectation, (string) $converter->convert($infix));
+  }
+
+  /**
+   * @dataProvider dataProviderExpression
+   */
+  public function testExpression($values,$expectation) {
+    $expression = new Expression($values);
+    $this->assertEquals($expectation, $expression->isValid());
+  }
+
+  public function dataProviderExpression() {
+    return array(
+      array(array(5, 3, '+'), true),
+      array(array(5, '+', 3), false),
+    );
   }
 
   public function dataProviderConversion() {
     return array(
-      array('1', '1'),
       array('2 3 +', '2 + 3'),
-      array('2 3 5 + +', '2 + 3 + 5'),
-      array('2 3 5 + *', '2 * (3 + 5)'),
-      array('2 3 5 + *', '2 * (3 + 5)'),
+#      array('2 3 5 + +', '2 + 3 + 5'),
+#      array('2 3 5 + *', '2 * (3 + 5)'),
+#      array('2 3 5 + *', '2 * (3 + 5)'),
 #      array('56 34 213.7 + * 678 -', '56 * (34 + 213.7) - 678'),
     );
   }
